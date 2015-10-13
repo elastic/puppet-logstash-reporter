0fc34f52f1b4dff8111c36d71265dc2fdac48584
Jason Cochard <jasonc@uber.com>
2015-10-13 07:51:23 -0700

    fix test

--- spec/classes/logstash_reporter_init_spec.rb
+++ spec/classes/logstash_reporter_init_spec.rb
@@ -13,7 +13,7 @@ describe 'logstash_reporter', :type => :class do
     let(:facts) { { :is_pe => true } }
     it { should contain_class('logstash_reporter') }
     it { should contain_class('logstash_reporter::params') }
-    it { should contain_file('/etc/puppetlabs/puppet/logstash.yaml').with(:owner => 'pe_puppet') }
+    it { should contain_file('/etc/puppetlabs/puppet/logstash.yaml').with(:owner => 'pe-puppet') }
   end
 
   context 'puppet 4' do
@@ -23,4 +23,3 @@ describe 'logstash_reporter', :type => :class do
     it { should contain_file('/etc/puppetlabs/puppet/logstash.yaml').with(:owner => 'puppet') }
   end
 end
-
